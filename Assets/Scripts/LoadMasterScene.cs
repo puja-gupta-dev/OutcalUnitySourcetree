9774da5aeffe15609e530b79381e742fc507704f
evobiunity <61403497+evobiunity@users.noreply.github.com>
2020-06-13 12:08:27 +0530

    Added feature1-buttons

---
+++ Assets/Scripts/LoadMasterScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadMasterScene : MonoBehaviour
+{
+    [SerializeField]
+    Button _button;
+
+    [SerializeField]
+    string _sceneName;
+
+    private void Start()
+    {
+        _button.onClick.AddListener(() => OnBackButtonClick());
+    }
+
+    void OnBackButtonClick()
+    {
+        SceneManager.LoadScene(_sceneName);
+    }
+}
