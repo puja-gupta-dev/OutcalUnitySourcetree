94018ec87e0884dfb6dcd096f1eec080f34b39cd
evobiunity <61403497+evobiunity@users.noreply.github.com>
2020-06-16 15:17:30 +0530

    Challenge 2 animations

---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            _animator.SetBool("isCrouch", true);
+        }
+    }
+}
